d6ef3fe5daab5e58812780e588117eef8f8081ff
angit <angit@Chikkus_hp>
2023-03-27 13:52:55 +0530

    Added functions for action class and drop-down

--- framework/src/main/java/com/utilities/ActionUtils.java
+++ framework/src/main/java/com/utilities/ActionUtils.java
@@ -3,12 +3,229 @@ package com.utilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class ActionUtils {
 
 	WebDriver driver;
 	WebElement element;
 	
+	/**
+	 * To click the element using action class
+	 * @param driver
+	 * @param element
+	 */
+	public void toClickTheElementUsingActionClass(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.click(element).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * To hover mouse over an element
+	 * @param driver
+	 * @param element
+	 */
+	public void toHoverMouseOverElement(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveToElement(element).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to move pointer to an element and click
+	 * @param driver
+	 * @param element
+	 */
+	public void toMoveToElementAndClick(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveToElement(element).click().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to right click
+	 * @param driver
+	 * @param element
+	 */
+	public void toRightClick(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.contextClick(element).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to move pointer to an element and right click
+	 * @param driver
+	 * @param element
+	 */
+	public void toMoveToElementAndRightClick(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveToElement(element).contextClick().build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to double click
+	 * @param driver
+	 * @param element
+	 */
+	public void toDoubleClick(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.doubleClick(element).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to move to an element and double click
+	 * @param driver
+	 * @param element
+	 */
+	public void toMoveToElementAndDoubleClick(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveToElement(element).doubleClick().build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to click and hold an element
+	 * @param driver
+	 * @param element
+	 */
+	public void toClickAndHold(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.clickAndHold(element);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to move to the element and then click and hold
+	 * @param driver
+	 * @param element
+	 */
+	public void toMoveToElementAndClickAndHold(WebDriver driver,WebElement element)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveToElement(element).clickAndHold().build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to move to an element to the given x & y co-ordinates
+	 * @param driver
+	 * @param x
+	 * @param y
+	 */
+	public void toMoveToElementByOffset(WebDriver driver,int x,int y)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.moveByOffset(x,y).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	/**
+	 * This method is to drag and drop
+	 * @param driver
+	 * @param drag
+	 * @param drop
+	 */
+	public void toDragAndDrop(WebDriver driver,WebElement drag,WebElement drop)
+	{
+		Actions action=new Actions(driver);
+		try 
+		{
+			action.dragAndDrop(drag, drop).build().perform();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
+	
+	
+	/**
+	 * This method is to scroll down the page
+	 * @param driver
+	 */
 	public void toScrollDownThePage(WebDriver driver)
 	{
 	try 
@@ -23,6 +240,10 @@ public class ActionUtils {
 	}
 	}
 	
+	/**
+	 * This method is to scroll up the page
+	 * @param driver
+	 */
 	public void toScrollUpThePage(WebDriver driver)
 	{
 	try 
@@ -37,7 +258,11 @@ public class ActionUtils {
 	}
 	}
 	
-	
+	/**
+	 * This method is to scroll the page to a particular element
+	 * @param driver
+	 * @param element
+	 */
 	public void toScrollToTheElement(WebDriver driver,WebElement element)
 	{
 	try 
@@ -52,6 +277,11 @@ public class ActionUtils {
 	}
 	}
 	
+	/**
+	 * This method is to click the element
+	 * @param driver
+	 * @param element
+	 */
 	public void toClickTheElement(WebDriver driver,WebElement element)
 	{
 	try 
--- framework/src/main/java/com/utilities/GenericUtils.java
+++ framework/src/main/java/com/utilities/GenericUtils.java
@@ -2,6 +2,7 @@ package com.utilities;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.OutputType;
@@ -20,7 +21,12 @@ public class GenericUtils {
 	WebDriver driver;
 	Select select;
 	
-	
+	/**
+	 * To select value from drop-down using value
+	 * @param driver
+	 * @param element
+	 * @param value
+	 */
 	public void selectByValue(WebDriver driver,WebElement element,String value)
 	{
 		select =new Select(element);
@@ -34,6 +40,12 @@ public class GenericUtils {
 		}	
 	}
 	
+	/**
+	 * To select value from drop-down using index
+	 * @param driver
+	 * @param element
+	 * @param index
+	 */
 	public void selectByIndex(WebDriver driver,WebElement element,int index)
 	{
 		Select select =new Select(element);
@@ -47,32 +59,253 @@ public class GenericUtils {
 		}
 	}
 	
+	/**
+	 * To select value from drop-down using visible text
+	 * @param driver
+	 * @param element
+	 * @param text
+	 */
+	public void selectByVisibleText(WebDriver driver,WebElement element,String text)
+	{
+		Select select=new Select(element);
+		try 
+		{
+			select.selectByVisibleText(text);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+	}
 	
+	/**
+	 * This method is to get all options in the drop-down
+	 * @param driver
+	 * @param element
+	 * @return
+	 */
+	public List<WebElement> toGetAllOptionsInDropdown(WebDriver driver,WebElement element)
+	{
+		List<WebElement> option=null;
+		Select select=new Select(element);
+		try
+		{
+			option=select.getOptions();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+		return option;
+	}
 	
+	/**
+	 * To get all the selected options in a drop-down
+	 * @param driver
+	 * @param element
+	 * @return
+	 */
+	public List<WebElement> toGetAllSelectedOptions(WebDriver driver,WebElement element)
+	{
+		List<WebElement> option = null;
+		Select select=new Select(element);
+		try 
+		{
+			option = select.getAllSelectedOptions();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+		return option;
+	}
+	
+	/**
+	 * To get the first select option in the drop-down
+	 * @param driver
+	 * @param element
+	 * @return
+	 */
+	public String toGetTheFirstSelectedOption(WebDriver driver,WebElement element)
+	{
+		String option = null;
+		Select select=new Select(element);
+		try 
+		{
+			option=select.getFirstSelectedOption().getText();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}		
+		return option;
+	}
+	
+	/**
+	 * To de-select option from drop-down using value
+	 * @param driver
+	 * @param element
+	 * @param value
+	 */
+	public void deSelectOptionByValue(WebDriver driver,WebElement element,String value)
+	{
+		Select select=new Select(element);
+		try 
+		{
+			select.deselectByValue(value);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());	
+		}
+	}
+	
+	/**
+	 * To de-select option from drop-down by index
+	 * @param driver
+	 * @param element
+	 * @param index
+	 */
+	public void deSelectOptionByIndex(WebDriver driver,WebElement element,int index)
+	{
+		Select select=new Select(element);
+		try 
+		{
+			select.deselectByIndex(index);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());	
+		}
+	}
+
+	/**
+	 * To de-select option from drop-down by visible text
+	 * @param driver
+	 * @param element
+	 * @param value
+	 */
+	public void deSelectOptionByVisibleText(WebDriver driver,WebElement element,String value)
+	{
+		Select select=new Select(element);
+		try 
+		{
+			select.deselectByVisibleText(value);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());	
+		}
+	}
+	
+	/**
+	 * To de-select all selected options from drop-down
+	 * @param driver
+	 * @param element
+	 */
+	public void deSelectAllSelectedOptions(WebDriver driver,WebElement element)
+	{
+		Select select=new Select(element);
+		try 
+		{
+			select.deselectAll();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());	
+		}	
+	}
+	
+	/**
+	 * This method is to dismiss an alert
+	 * @param driver
+	 */
 	public void toDismissAnAlert(WebDriver driver)
 	{
 		Alert alert=driver.switchTo().alert();
-		alert.dismiss();
+		try 
+		{
+			alert.dismiss();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
 	}
 	
+	/**
+	 * This method is to accept an alert
+	 * @param driver
+	 */
 	public void toAcceptAnAlert(WebDriver driver)
 	{
 		Alert alert=driver.switchTo().alert();
-		alert.accept();
+		try 
+		{
+			alert.accept();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
 	}
 	
+	/**
+	 * This method is to enter value to an alert
+	 * @param driver
+	 * @param value
+	 */
 	public void toEnterValueToTheAlert(WebDriver driver,String value)
 	{
 		Alert alert=driver.switchTo().alert();
-		alert.sendKeys(value);
+		try 
+		{
+			alert.sendKeys(value);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
 	}
 	
-	public void toGetTitleOfTheAlert()
-	{
+	/**
+	 * This method is to get the title of the alert
+	 * @return 
+	 */
+	public String toGetTitleOfTheAlert(WebDriver driver)
+	{	
+		String text=null;
 		Alert alert=driver.switchTo().alert();
-		alert.getText();
+		try 
+		{
+			text=alert.getText();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+		return text;
 	}
 	
+	/**
+	 * This method is to capture the screenshot of a page
+	 * @param driver
+	 * @param path
+	 * @param filename
+	 * @throws IOException
+	 */
 	public void toCaptureScreenshot(WebDriver driver, String path, String filename) throws IOException
 	{
 		try {
